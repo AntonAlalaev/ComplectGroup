4098db294bdafc223067cd68b895dea46faa04e4
Anton Alalaev <antonalalaev@gmail.com>
2025-12-09 17:55:06 +0500

    Еще немного подправил и отредактировал просмотр списка комплектаций.

--- ComplectGroup.Application/obj/Debug/net8.0/ComplectGroup.Application.AssemblyInfo.cs
+++ ComplectGroup.Application/obj/Debug/net8.0/ComplectGroup.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComplectGroup.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ebb2d7dffa2bfac0eb5345b82deac10d1a88832")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fa7e0d926d2b2265f529f05cc45c4f0e45de1b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComplectGroup.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComplectGroup.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
